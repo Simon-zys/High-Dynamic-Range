74c3bca31a0828d8e32315faa9a06cf1e101853d
Max <clotha87762@yahoo.com.tw>
2018-04-11 01:03:11 +0800

    PaulDebevec bug version

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2
 from Robertson import *
 from tonemapping import *
+from PaulDebevec import *
+import sys
 
 def readfromfile(path, filename, scale):
     with open(filename, 'r') as f:
@@ -25,51 +27,57 @@ def readfromfile(path, filename, scale):
     exp_times = np.array(exp_times, dtype='float32')
     return images, exp_times
 
-images, exp_times = readfromfile('test/', 'test/memorial.hdr_image_list.txt', 1)
-# images, exp_times = readfromfile('HDR_data/', 'HDR_data/input.txt', 0.15 )
 
-# Remove blue backgrounds
-# height, width, channel = images[0].shape
-# for c in range(len(images)):
-#     for i in range(height):
-#         for j in range(width):
-#             if np.all(images[c][i][j] == [0,  0,  255]):
-#                 images[c][i][j] = [0,  0,  0]
-#                 print('hi', end='\r')
+if __name__ == '__main__':
 
 
-recovery = RobertsonHDR()
-recover_hdr = recovery.process(images, exp_times)
 
-"""
-E = recover_hdr
-cv2.imwrite("Robertson_myhdr_mini.hdr", E[...,[2,1,0]]) 
-ldrDrago = tonemapping(E, 0.5, 1.0) 
-t = np.clip(ldrDrago*255, 0, 255).astype('uint8')
-cv2.imwrite("Robertson_mytonemap_mini.jpg", t[...,[2,1,0]] )
+	images, exp_times = readfromfile('test/', 'test/memorial.hdr_image_list.txt', 1)
+	# images, exp_times = readfromfile('HDR_data/', 'HDR_data/input.txt', 0.15 )
 
-print('finish recover mini HDR')
+	# Remove blue backgrounds
+	# height, width, channel = images[0].shape
+	# for c in range(len(images)):
+	#     for i in range(height):
+	#         for j in range(width):
+	#             if np.all(images[c][i][j] == [0,  0,  255]):
+	#                 images[c][i][j] = [0,  0,  0]
+	#                 print('hi', end='\r')
 
-images, exp_times = readfromfile('HDR_data/', 'HDR_data/input.txt', 1)
 
-height, width, channel = images[0].shape
-hdr = np.zeros([height, width, 3], dtype='float32')
-for c in range(3): 
-    for i in range(height):
-        for j in range(width):
-            u = 0.0
-            b = 0.0
-            for n in range(len(images)):
-                z = images[n][i,j,c]
-                t = exp_times[n]
-                u += recovery.w[z] * recovery.res_curve[c,z] * t
-                b += recovery.w[z] * t * t
-            hdr[i,j,c] = u/b
-    print('finish channel', c)
-"""
+	#recovery = RobertsonHDR()
+	recovery = PaulDebevecHDR()
+	recover_hdr = recovery.AssembleHDR(images, exp_times)
 
-E = recover_hdr
-cv2.imwrite("Robertson_myhdr_full.hdr", E[...,[2,1,0]]) 
-ldrDrago = tonemapping(E, 0.5, 1.0) 
-t = np.clip(ldrDrago*255, 0, 255).astype('uint8')
-cv2.imwrite("Robertson_mytonemap_full.jpg", t[...,[2,1,0]] )
+	"""
+	E = recover_hdr
+	cv2.imwrite("Robertson_myhdr_mini.hdr", E[...,[2,1,0]]) 
+	ldrDrago = tonemapping(E, 0.5, 1.0) 
+	t = np.clip(ldrDrago*255, 0, 255).astype('uint8')
+	cv2.imwrite("Robertson_mytonemap_mini.jpg", t[...,[2,1,0]] )
+
+	print('finish recover mini HDR')
+
+	images, exp_times = readfromfile('HDR_data/', 'HDR_data/input.txt', 1)
+
+	height, width, channel = images[0].shape
+	hdr = np.zeros([height, width, 3], dtype='float32')
+	for c in range(3): 
+	    for i in range(height):
+	        for j in range(width):
+	            u = 0.0
+	            b = 0.0
+	            for n in range(len(images)):
+	                z = images[n][i,j,c]
+	                t = exp_times[n]
+	                u += recovery.w[z] * recovery.res_curve[c,z] * t
+	                b += recovery.w[z] * t * t
+	            hdr[i,j,c] = u/b
+	    print('finish channel', c)
+	"""
+
+	E = recover_hdr
+	cv2.imwrite("PaulDebevec_myhdr_full.hdr", E[...,[2,1,0]]) 
+	ldrDrago = tonemapping(E, 0.5, 1.0) 
+	t = np.clip(ldrDrago*255, 0, 255).astype('uint8')
+	cv2.imwrite("PaulDebevec_mytonemap_full.jpg", t[...,[2,1,0]] )
